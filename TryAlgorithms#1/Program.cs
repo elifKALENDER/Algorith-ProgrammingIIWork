8a067a91586fd9fec885b859cfeec51870c34c80
elif kalender <elifkalender.tr@gmail.com>
2023-03-29 00:03:14 +0300

    first commit

---
+++ TryAlgorithms#1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TryAlgorithms_lib_cs;
+
+namespace TryAlgorithms_1 {
+    internal class Program {
+        static void Main(string[] args) {
+            /* RdmVal=Random.Equals(new Random().Next());*/
+            Random rdmVal = new Random();
+            rdmVal.Next(0, 30);
+            HeapSort(rdmVal) = HSort;
+            Console.WriteLine(HSort);
+        }
+    }
+}
